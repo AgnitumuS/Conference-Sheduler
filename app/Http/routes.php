a366da575c57877a3542cd4eb31032a756ae463f
ivo_leng4 <ivo_leng4@SYS-VEN.voenet.local>
2016-07-28 14:14:07 +0300

    Объединил маршруты в группу

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,26 +1,27 @@
 <?php
 
 use App\Event;
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
-Route::get('/', 'EventController@index');
+/*
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 Route::auth();
-Route::resource('event', 'EventController');
-Route::resource('room', 'RoomController');
-
-Route::get('settings', 'SettingsController@index');
-Route::post('settings', 'SettingsController@store');
-Route::get('/ldap', function(){
-  $a = Adldap::getDefaultProvider()->search()->users()->find('Вершков');
-  dd($a);
+Route::get('/', 'EventController@index');
+Route::group(['middleware' => 'auth'], function() {
+    Route::resource('event', 'EventController');
+    Route::resource('room', 'RoomController');
+    Route::get('settings', 'SettingsController@index');
+    Route::post('settings', 'SettingsController@store');
+    Route::get('/ldap', function() {
+        $a = Adldap::getDefaultProvider()->search()->users()->find('Вершков');
+        dd($a);
+    });
 });
