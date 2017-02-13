46c2ccb91d2aa7188870868e5a2b9f20a661f8fe
ivo_leng4 <ivo_leng4@SYS-VEN.voenet.local>
2017-02-13 09:44:29 +0300

    add 404 route

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1" >
+                    <div class="jumbotron">
+                        <h1>404 Страница не найдена</h1>
+                        <p>Страница, на которую вы пытаетесь попасть, вероятно не существует</p>
+                        <p><a class="btn btn-default btn-lg" href="/" role="button">на главную</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
